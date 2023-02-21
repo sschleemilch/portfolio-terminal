1a5eb3b5b098eb680ba2543bd1c886224f91e666
Sebastian Schleemilch <sebastian.schleemilch@gmail.com>
2023-02-21 22:58:41 +0100

    init

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,36 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+      <meta charSet="utf-8" />
+          <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+          <link
+            rel="apple-touch-icon"
+            sizes="57x57"
+            href="/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="192x192"
+            href="/android-chrome-192x192.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
+          <meta name="msapplication-TileColor" content="#262626" />
+          <meta name="theme-color" content="#D79921" />
+          <meta name="description" content="Portfolio | Sebastian Schleemilch" />
+      </Head>
       <body>
         <Main />
         <NextScript />
