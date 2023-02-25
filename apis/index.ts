d9b432915cdbaa8ae938aec2b0c4d2fa1ff24619
Sebastian Schleemilch <sebastian.schleemilch@gmail.com>
2023-02-25 12:28:42 +0100

    Nord, cowsay, axios, weather

---
+++ apis/index.ts
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+export const getQuote = async () => {
+    const { data } = await axios.get('https://api.quotable.io/random');
+  
+    return {
+      quote: `“${data.content}” — ${data.author}`,
+    };
+};
+
+export const getWeather = async (city: string) => {
+  const { data } = await axios.get(`https://wttr.in/${city}?ATm`);
+
+  return data;
+};
--- utils/bin/index.tsx
+++ utils/bin/index.tsx
@@ -1,3 +1,5 @@
 export * from './neofetch';
 export * from './theme';
 export * from './utils';
+export * from './cowsay'
+export * from './weather'
